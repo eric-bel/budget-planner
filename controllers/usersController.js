47951e60e6caa91ebaaa4fd583184f93b2e987b7
eric bel <ericbelecov@gmail.com>
2022-03-19 05:26:15 +0400

    created method to CREATE data in database

--- controllers/usersController.js
+++ controllers/usersController.js
@@ -3,8 +3,16 @@ const User = require("../models/User");
 
 async function getUsers(req, res) {
   const users = await User.find({});
-  console.log(users);
   res.send(users);
 }
 
-module.exports = {getUsers};
+async function createUser(req, res) {
+  try {
+    const user = await User.create(req.body);
+    res.status(200).json(user);
+  } catch (err) {
+    res.status(400).json(err);
+  }
+}
+
+module.exports = { getUsers, createUser };
--- routes/usersRouter.js
+++ routes/usersRouter.js
@@ -1,7 +1,10 @@
 const Router = require("express");
-const { getUsers } = require("../controllers/usersController");
+const { getUsers, createUser } = require("../controllers/usersController");
 const router = new Router();
+const User = require("../models/User");
+
 
 router.get("/getusers", getUsers);
+router.post("/create", createUser);
 
 module.exports = router;
