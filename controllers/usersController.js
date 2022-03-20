b7b32c66cf2470e1109efe2df814b08ea14a16ba
eric bel <ericbelecov@gmail.com>
2022-03-20 10:22:43 +0400

    homework 3 done

--- controllers/usersController.js
+++ controllers/usersController.js
@@ -1,4 +1,3 @@
-const mongoose = require("mongoose");
 const User = require("../models/User");
 
 async function getUsers(req, res) {
--- routes/authRouter.js
+++ routes/authRouter.js
@@ -1,15 +1,14 @@
 const Router = require("express");
 const controller = require("../controllers/authController");
-// const passport = require("passport");
 
 const router = new Router();
 
 router.post("/registration", controller.registration);
 
-router.post(
-  "/login",
-  // passport.authenticate("jwt", { session: false }),
-  controller.login
-);
+router.post("/login", controller.login);
 
 module.exports = router;
+
+// can be used
+// const passport = require("passport");
+// passport.authenticate("jwt", { session: false }),
