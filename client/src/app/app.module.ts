3772f9ffb02282ce9e61dae0f219521a1efb577d
eric bel <ericbelecov@gmail.com>
2022-04-03 19:11:29 +0400

    layouts

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -4,11 +4,11 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { AuthComponent } from './auth/auth.component';
-import { TestComponent } from './test/test.component';
 import { AuthLayoutComponent } from './shared/layouts/auth-layout/auth-layout.component';
+import { SiteLayoutComponent } from './shared/layouts/site-layout/site-layout.component';
 
 @NgModule({
-  declarations: [AppComponent, AuthComponent, TestComponent, AuthLayoutComponent],
+  declarations: [AppComponent, AuthComponent, AuthLayoutComponent, SiteLayoutComponent],
   imports: [BrowserModule, AppRoutingModule],
   providers: [],
   bootstrap: [AppComponent],
