adbb929f318ee30e669d14cddfea4545d0d37133
eric bel <ericbelecov@gmail.com>
2022-04-04 02:57:26 +0400

    creating auth service

---
+++ client/src/app/shared/services/auth.service.ts
@@ -0,0 +1,22 @@
+import { Injectable } from '@angular/core';
+
+@Injectable()
+export class AuthService {}
+
+//   constructor(private http: HttpClient) {}
+//
+//   getUser() {
+//     return this.http.get(`${environment.apiUrl}/auth/me`);
+//   }
+
+//   login(user: User) {
+//     return this.http.post(`${environment.apiUrl}/auth/login`, user);
+//   }
+
+//   logout() {
+//     return this.http.delete(`${environment.apiUrl}/auth/logout`);
+//   }
+
+//   register(user: User) {
+//     return this.http.post(`${environment.apiUrl}/auth/register`, user);
+//   }
