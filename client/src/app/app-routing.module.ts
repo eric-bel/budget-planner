18422e886900cf22106efbe5e0b93775c24030a6
eric bel <ericbelecov@gmail.com>
2022-04-03 22:06:56 +0400

    login layout done

--- client/src/app/app-routing.module.ts
+++ client/src/app/app-routing.module.ts
@@ -1,8 +1,17 @@
+import { SiteLayoutComponent } from './shared/layouts/site-layout/site-layout.component';
 import { AuthComponent } from './auth/auth.component';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AuthLayoutComponent } from './shared/layouts/auth-layout/auth-layout.component';
 
-const routes: Routes = [{ path: 'login', component: AuthComponent  }];
+const routes: Routes = [
+  {
+    path: '',
+    component: AuthLayoutComponent,
+    children: [{ path: 'login', component: AuthComponent }],
+  },
+  { path: '', component: SiteLayoutComponent, children: [] },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
